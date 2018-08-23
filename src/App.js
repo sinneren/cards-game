f04c0d77f71231ace71c73a6268631f3f99a63ee
sinneren <sinneren@gmail.com>
2018-08-23 15:26:20 +0500

    Basic UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button';
 
 class App extends Component {
+  handleClick(event) {
+    console.log(event)
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button text="Start the game!" handleClick={this.handleClick.bind(this)} />
       </div>
     );
   }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Button extends Component {
+    render() {
+        this.defaults = {
+            class: 'button',
+            text: 'Button Text'
+        }
+        return (
+            <button
+                className={(this.props.class) ? this.props.class : this.defaults.class}
+                onClick={this.props.handleClick}>
+                {(this.props.text) ? this.props.text : this.defaults.text}
+            </button>
+        );
+    }
+}
+
+export default Button;
